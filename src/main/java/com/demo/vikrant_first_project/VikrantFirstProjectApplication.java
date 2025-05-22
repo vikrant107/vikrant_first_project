bbf378f9af9aa782d086ed84906791b4790fa26f
vikrant107 <vikrantkadam70@gmail.com>
2025-05-22 08:06:23 -0700

    Added project

---
+++ src/main/java/com/demo/vikrant_first_project/VikrantFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.demo.vikrant_first_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikrantFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikrantFirstProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/vikrant_first_project/VikrantFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.vikrant_first_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikrantFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
